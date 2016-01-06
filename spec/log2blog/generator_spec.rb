0032b3f7bdc0cc2572f7dd7ead285174c161f3a7
Josh Justice <jjustice@bignerdranch.com>
2016-01-06 12:16:28 -0500

    switch examples to one-line

--- spec/log2blog/generator_spec.rb
+++ spec/log2blog/generator_spec.rb
@@ -5,12 +5,13 @@ module Log2Blog
     subject(:generator) { described_class.new( github ) }
     let(:github) { instance_double(GithubClient, history: commits) }
 
-    describe "#generate_markdown" do
+    describe "#generate_markdown( user, repo )" do
 
       subject { generator.generate_markdown( "TestUser", "TestRepo" ) }
 
       context "when there are no commits" do
         let(:commits) { [] }
+        
         it { should be_empty }
       end
 
@@ -25,21 +26,16 @@ module Log2Blog
       end
 
       context "when there is one commit with two files" do
-        let(:commits) { [commit] }
-        let(:commit) { FactoryGirl.build(:commit, files: files ) }
+        let(:commits) { [FactoryGirl.build(:commit, files: files )] }
         let(:files) { FactoryGirl.build_list(:commit_file, 2) }
 
-        it "includes the filenames in order" do
-          expect(subject).to contain_in_order( files.map(&:name) )
-        end
+        it { is_expected.to contain_in_order( files.map(&:name) ) }
       end
 
       context "when there are two commits" do
         let(:commits) { FactoryGirl.build_list( :commit, 2 ) }
 
-        it "includes the commit messages in order" do
-          expect(subject).to contain_in_order( commits.map(&:message) )
-        end
+        it { is_expected.to contain_in_order( commits.map(&:message) ) }
       end
     end
   end
